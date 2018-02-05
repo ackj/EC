961fc29b0dc65e796a3653c3ec9a294d4d048d08
langmanleguang <langmanleguang@qq.com>
2018-02-05 10:48:33 +0800

    配置ARouter

--- app/src/main/java/cn/itsite/ec/MainActivity.java
+++ app/src/main/java/cn/itsite/ec/MainActivity.java
@@ -1,13 +1,15 @@
 package cn.itsite.ec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cn.itsite.abase.mvp.view.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        loadRootFragment(android.R.id.content, new PayFragment());
     }
 }
